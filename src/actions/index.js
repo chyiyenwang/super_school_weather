62512f2503cf5c98496e39509e8095c623070021
chyiyenwang <chyiyenwang@gmail.com>
2016-05-05 13:54:21 -0700

    implemented fetch

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,14 @@
 const API_KEY = '14a9fdd858e7f446ebd55fe049c90bfb';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = fetch(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+};
