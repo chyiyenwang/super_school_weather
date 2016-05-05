3f06946bf72fbbced5445351f7a71981299e0eb0
chyiyenwang <chyiyenwang@gmail.com>
2016-05-05 13:04:30 -0700

    preventDefault added

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -12,9 +12,13 @@ export default class SearchBar extends Component {
     this.setState({term: event.target.value})
   }
 
+  onFormSubmit(event) {
+    event.preventDefault();
+  }
+
   render() {
     return (
-      <form className="input-group">
+      <form onSubmit={this.onFormSubmit} className="input-group">
         <input 
           placeholder="Get 5-day forcast"
           className="form-control"
